72c9563f331c3be5c140b2e87f716ba5971da1c0
Az3r <tuan99kghd@gmail.com>
2020-01-14 23:49:32 +0700

    use new Exception class to handle error

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,5 +1,6 @@
 #include "WinApp.h"
-#include <cstring>
+#include "Exception.h"
+
 LRESULT WinApp::SetupWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	WinApp* lpWinApp = nullptr;
@@ -22,33 +23,9 @@ ATOM WinApp::RegisterWndClassEx()
 	m_wc.lpfnWndProc = WinApp::SetupWndProc;
 	m_wc.lpszClassName = m_sClassName.c_str();
 
-
 	return RegisterClassEx(&m_wc);
 }
 
-inline void WinApp::Assert(BOOL condition, wchar_t* msg)
-{
-	// A BOOL value with 0 indicates failure and success for value that does not equal to 0
-	if (condition <= 0) MessageBox(NULL, msg, L"Error", MB_OK | MB_ICONWARNING);
-}
-
-inline void WinApp::Assert(HRESULT hr, wchar_t* msg)
-{
-	// An HRESULT value indicates success when it is >= 0, otherwise failure
-	if (FAILED(hr)) MessageBox(NULL, msg, L"Error", MB_OK | MB_ICONWARNING);
-}
-
-inline void WinApp::AssertWndFuncs(HRESULT hr)
-{
-	// An HRESULT value indicates success when it is >= 0, otherwise failure
-	if (FAILED(hr)) MessageBox(NULL, GetErrorMsg(GetLastError()).c_str(), L"Error", MB_OK | MB_ICONWARNING);
-}
-
-inline void WinApp::AssertWndFuncs(BOOL condition)
-{
-	// A BOOL value with 0 indicates failure and success for value that does not equal to 0
-	if (condition <= 0) MessageBox(NULL, GetErrorMsg(GetLastError()).c_str(), L"Error", MB_OK | MB_ICONWARNING);
-}
 
 WinApp::WinApp(HINSTANCE hInstance, int nCmdShow) : WinApp()
 {
@@ -58,8 +35,6 @@ WinApp::WinApp(HINSTANCE hInstance, int nCmdShow) : WinApp()
 
 int WinApp::InitInstance()
 {
-	AssertWndFuncs(RegisterWndClassEx());
-
 	m_hwnd = CreateWindowEx(
 		WS_EX_CLIENTEDGE,
 		m_sClassName.c_str(),
@@ -72,7 +47,8 @@ int WinApp::InitInstance()
 		this
 	);
 
-	AssertWndFuncs(reinterpret_cast<BOOL>(m_hwnd));
+	Exception::throw_if_false((BOOL)m_hwnd, Exception::TranslateErrorCode(GetLastError()), __LINE__, "Window Creation Failure");
+
 	ShowWindow(m_hwnd, m_nCmdShow);
 	UpdateWindow(m_hwnd);
 
@@ -103,7 +79,7 @@ int WinApp::Run()
 int WinApp::ExitInstance()
 {
 	BOOL result = UnregisterClass(m_sClassName.c_str(), m_hInstance);
-	AssertWndFuncs(result);
+	Exception::throw_if_false(result, Exception::TranslateErrorCode(GetLastError()), __LINE__, "Unregisteration Failure");
 	return 0;
 }
 
@@ -133,39 +109,4 @@ LRESULT WinApp::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
-}
-
-std::wstring WinApp::GetErrorMsg(DWORD nErrorCode)
-{
-	// get language id
-	DWORD value;
-	int langResult = GetLocaleInfoEx(L"en-us", LOCALE_ILANGUAGE | LOCALE_RETURN_NUMBER, reinterpret_cast<LPWSTR>(&value), sizeof(DWORD) / sizeof(wchar_t));
-	if (!langResult)
-	{
-		MessageBox(NULL, L"Unable to retreive language id", L"Error", MB_OK | MB_ICONERROR);
-		return std::wstring();
-	}
-	LANGID langId = value;
-
-	// get message corresponding its error code
-	LPTSTR pBuffer = nullptr;
-	DWORD formatResult = FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,	// dwFlags
-		NULL,																							// lpSource
-		nErrorCode,																						// dwMessageId
-		langId,																							// dwLanguageId
-		reinterpret_cast<LPTSTR>(&pBuffer),																// lpBuffer
-		0,																								// nSize
-		NULL																							// pArgs
-	);
-	if (!formatResult)
-	{
-		MessageBox(NULL, L"Unable to retreive format message", L"Error", MB_OK | MB_ICONERROR);
-		return std::wstring();
-	}
-
-	// store in an wstring instance and free buffer
-	std::wstring msg = std::wstring(pBuffer, wcslen(pBuffer));
-	LocalFree(pBuffer);
-	return msg;
 }
--- WinApp.h
+++ WinApp.h
@@ -16,18 +16,13 @@ private:
 	LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 	WinApp() : m_hwnd(NULL), m_hInstance(NULL), m_wc(), m_nCmdShow(NULL), m_sTitle(L"Desktop Application"), m_sClassName(L"DesktopClass") {}
 	ATOM RegisterWndClassEx();
+
 public:
 	WinApp(HINSTANCE hInstance, int nCmdShow);
 	~WinApp() {}
 	int InitInstance();
 	int Run();
 	int ExitInstance();
-	static std::wstring GetErrorMsg(DWORD nErrorCode);
-	static void Assert(BOOL condition, wchar_t* msg);
-	static void Assert(HRESULT hr, wchar_t* msg);
 
-	// Asserts Windows API methods' result with error code returned from GetLastError()
-	static void AssertWndFuncs(HRESULT hr);
-	static void AssertWndFuncs(BOOL condition);
 };
 
